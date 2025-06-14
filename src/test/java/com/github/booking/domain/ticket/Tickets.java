43005a511ddd083e2aac7e56a2dd636c8c552fcb
mtamme <mtamme@users.noreply.github.com>
2025-06-14 22:07:04 +0200

    feat: issue ticket for a booked seat

---
+++ src/test/java/com/github/booking/domain/ticket/Tickets.java
@@ -0,0 +1,35 @@
+package com.github.booking.domain.ticket;
+
+import com.github.booking.domain.hall.SeatNumber;
+import com.github.booking.domain.show.Shows;
+import com.github.seedwork.core.util.Consumers;
+
+public final class Tickets {
+
+  private Tickets() {
+  }
+
+  public static Ticket newRedeemedTicket(final String showId,
+                                         final String bookingId,
+                                         final String seatNumber,
+                                         final String ticketId) {
+    final var ticket = newIssuedTicket(showId, bookingId, seatNumber, ticketId);
+
+    ticket.redeem();
+    ticket.dispatchEvents(Consumers.empty());
+
+    return ticket;
+  }
+
+  public static Ticket newIssuedTicket(final String showId,
+                                       final String bookingId,
+                                       final String seatNumber,
+                                       final String ticketId) {
+    final var show = Shows.newShowWithBookedSeats(showId, bookingId, seatNumber);
+    final var ticket = show.issueTicket(new TicketId(ticketId), new SeatNumber(seatNumber));
+
+    ticket.dispatchEvents(Consumers.empty());
+
+    return ticket;
+  }
+}
