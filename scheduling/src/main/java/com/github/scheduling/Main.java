45212363a42c25e9e7f99d444ce7ad77f5fbeb53
mtamme <mtamme@users.noreply.github.com>
2026-03-26 23:41:39 +0100

    feat: rename main class

--- scheduling/src/main/java/com/github/scheduling/Main.java
+++ scheduling/src/main/java/com/github/scheduling/Main.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchedulingApplication {
+public class Main {
 
-  public static void main(final String[] args) {
-    SpringApplication.run(SchedulingApplication.class, args);
+  static void main(final String[] args) {
+    SpringApplication.run(Main.class, args);
   }
 }
